2d633f4d07c2b46380cc4040965cb9812d34555a
Ana <stanescuanamaria22@yahoo.com>
2021-12-02 21:27:57 +0200

    Expose MutableLiveData state as LiveData

--- app/src/main/java/com/ana/stanescu/foodmenu/feature/categories/FoodCategoriesViewModel.kt
+++ app/src/main/java/com/ana/stanescu/foodmenu/feature/categories/FoodCategoriesViewModel.kt
@@ -1,22 +1,32 @@
 package com.ana.stanescu.foodmenu.feature.categories
 
+import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ana.stanescu.foodmenu.model.FoodMenuRepository
 import com.ana.stanescu.foodmenu.model.response.FoodCategory
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
 class FoodCategoriesViewModel(private val repository: FoodMenuRepository = FoodMenuRepository()) :
     ViewModel() {
 
-    val state: MutableLiveData<FoodCategoriesState> =
+    private val errorHandler = CoroutineExceptionHandler { _, throwable ->
+        throwable.printStackTrace()
+    }
+
+    private val _state: MutableLiveData<FoodCategoriesState> =
         MutableLiveData(FoodCategoriesState(true, listOf()))
 
+    val state: LiveData<FoodCategoriesState>
+        get() = _state
+
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(errorHandler) {
             val categories = repository.getFoodCategories().categories
-            state.value = FoodCategoriesState(false, categories)
+            _state.value = FoodCategoriesState(false, categories)
         }
     }
 }
