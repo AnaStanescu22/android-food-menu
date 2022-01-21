42cf181a188b6fe4df0ff274504b3e73b5277b49
devhusnain <husnain.progz@gmail.com>
2022-01-21 18:39:04 +0000

    Husnain - Build Types Added

--- app/src/main/java/com/ana/stanescu/foodmenu/model/FoodMenuRepository.kt
+++ app/src/main/java/com/ana/stanescu/foodmenu/model/FoodMenuRepository.kt
@@ -1,16 +1,16 @@
 package com.ana.stanescu.foodmenu.model
 
+import com.ana.stanescu.foodmenu.BuildConfig
 import com.ana.stanescu.foodmenu.FoodMenuApplication
-import com.ana.stanescu.foodmenu.model.response.FoodCategoriesResponse
-import com.ana.stanescu.foodmenu.model.response.FoodCategory
 import com.ana.stanescu.foodmenu.data.FoodCategoryDao
 import com.ana.stanescu.foodmenu.data.FoodCategoryRoomDatabase
+import com.ana.stanescu.foodmenu.model.response.FoodCategoriesResponse
+import com.ana.stanescu.foodmenu.model.response.FoodCategory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 class FoodMenuRepository {
-    private val API_URL = "https://www.themealdb.com/api/json/v1/1/"
 
     private var service: Service
     private var dao: FoodCategoryDao =
@@ -20,7 +20,7 @@ class FoodMenuRepository {
         val retrofit: Retrofit = Retrofit
             .Builder()
             .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(API_URL)
+            .baseUrl(BuildConfig.API_URL)
             .build()
         service = retrofit.create(Service::class.java)
     }
